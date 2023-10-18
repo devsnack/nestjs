484b66fa23a5d4f09039dc6fd88412e6bc6379d5
devsnack <devsnack27@gmail.com>
2023-10-18 12:43:54 +0200

    postgres final

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { HttpExceptionFilterFilter } from './common/filters/http-exception.filter/http-exception.filter.filter';
-
+import { SwaggerModule, DocumentBuilder } from 'swagger-ui-express';
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.useGlobalPipes(
@@ -13,6 +13,16 @@ async function bootstrap() {
 			transform: true
 		})
 	);
+	//
+	const config = new DocumentBuilder()
+		.setTitle('Cats example')
+		.setDescription('The cats API description')
+		.setVersion('1.0')
+		.addTag('cats')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+	//
 	await app.listen(3000);
 	app.useGlobalFilters(new HttpExceptionFilterFilter());
 }
